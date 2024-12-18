6dfbfee02e0255e73813147e95768633829e9863
hossein ghaniamti <hosein.ghanimati1398@gmail.com>
2024-12-18 15:56:29 +0330

    Refactor Modal To Redux State

--- src/components/modals/Overlay.tsx
+++ src/components/modals/Overlay.tsx
@@ -1,13 +1,22 @@
-import { OverlayContext } from "@/contexts/share/overlay.context"
-import { memo, useContext } from "react"
-
+import { setModalAction } from "@/redux/modal/modal";
+import { AppDispatch, RootStates } from "@/redux/store";
+import { memo } from "react";
+import { useDispatch, useSelector } from "react-redux";
 
 const Overlay = memo(() => {
-  const overlaySetting = useContext(OverlayContext)
-
+  const overlaySetting = useSelector((store: RootStates) => store.overlay);
+  const dispath: AppDispatch = useDispatch();
   return (
-    <div id="overlay" onClick={() => overlaySetting?.hideOverlay()} className={`z-30 w-full  h-full fixed top-0 right-0 bg-opacity-30  backdrop-blur-sm bg-gray-500 ${overlaySetting?.isOpen ? "" : "hidden"}`}></div>
-  )
-})
+    <div
+      id="overlay"
+      onClick={() => dispath(
+        setModalAction(false)
+      )}
+      className={`z-30 w-full  h-full fixed top-0 right-0 bg-opacity-30  backdrop-blur-sm bg-gray-500 ${
+        overlaySetting?.isOpen ? "" : "hidden"
+      }`}
+    ></div>
+  );
+});
 
-export default Overlay
+export default Overlay;
